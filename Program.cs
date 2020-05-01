08281c5efe79b243904099169999f31a5c095500
Ben Abraham <ben.d.abraham@gmail.com>
2020-05-01 00:36:51 -0400

    Removed development appsettings file from repository, and actually added it to init. Removed keys from appsettings.

--- Program.cs
+++ Program.cs
@@ -23,6 +23,9 @@ namespace ADS.Bot1
                 .ConfigureAppConfiguration((hostingContext, config) =>
                 {
                     config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+#if DEBUG
+                    config.AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true);
+#endif
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
