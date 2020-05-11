14bc4e449b3a29a25696ee8c3d2e0b604b2710cc
Ben Abraham <ben.d.abraham@gmail.com>
2020-05-11 00:56:02 -0400

    Added ADS Sync project, an azure function app which syncs information from dealer API's nightly.

---
+++ Services/VinSolutionsService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADS.Bot.V1.Services
+{
+    public class VinSolutionsService
+    {
+        public VinSolutionsService()
+        {
+
+        }
+    }
+}
